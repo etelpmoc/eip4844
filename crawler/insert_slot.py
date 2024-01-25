a90860476fdb19b82be47ab330a96528506d3149
etelpmoc <etelmok@gmail.com>
2024-01-25 15:03:11 +0900

    Modify insert_slot.py to update recent blocks

--- crawler/insert_slot.py
+++ crawler/insert_slot.py
@@ -1,24 +1,31 @@
 import paths
 from eip4844.setting import *
 from pymongo import MongoClient
+import requests
 
 if __name__ == "__main__":
-    start = int(sys.argv[1])
-    end   = int(sys.argv[2])
     
     client = MongoClient(f'mongodb://{MONGODB_USER}:{MONGODB_PASSWORD}@localhost:27017/')
-    r = requests.get(f"http://localhost:3500/eth/v2/beacon/blocks/8234586")
     db = client['ethereum']
     collection = db['slots']
     
+    r = requests.get(f"http://localhost:{BEACON_PORT_NUM}/eth/v1/beacon/headers")
+    
+    start = collection.find_one(sort=[("slot", -1)])['slot']+1
+    end   = int(json.loads(r.text)['data'][0]['header']['message']['slot'])
+    
+    print(f"Starting updates from Slot #{start} to #{end}")
     
     documents = []
     for slot in range(start, end):
         r = requests.get(f"http://localhost:{BEACON_PORT_NUM}/eth/v2/beacon/blocks/{slot}")
-        message = json.loads(r.text)['data']['message']
+        try:
+            message = json.loads(r.text)['data']['message']
+        except:
+            continue            
 
         documents.append({'slot':slot,
-                          'block':message['execution_payload']['block_number'],
+                          'block':message['body']['execution_payload']['block_number'],
                           'proposer_index':message['proposer_index'],
                           'attestation_length':len(message['body']['attestations']),
                           'fee_recipient':message['body']['execution_payload']['fee_recipient']
